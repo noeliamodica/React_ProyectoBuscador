426208d12feddda4a8494a8dea0b645fc968d1fe
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-05 21:27:36 -0300

    genero base de datos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,94 @@
 
 import './App.css';
 
+//simular un conjunto de datos
+const people =[
+  {
+    id: 'people-01',
+    title: 'Juan Perez',
+
+  },
+  {
+    id: 'people-02',
+    title: 'Marcos Rivas',
+
+  },
+  {
+    id: 'people-03',
+    title: 'Sergio Martinez',
+
+  },
+  {
+    id: 'people-04',
+    title: 'Laura Jimenez',
+
+  },
+  {
+    id: 'people-05',
+    title: 'Horacio Martinez',
+
+  },
+
+];
+
+const calendar =[
+  {
+    id: 'calendar-01',
+    title: 'Sesion de seguimiento',
+
+  },
+  {
+    id: 'calendar-02',
+    title: 'Revision de propuestas',
+
+  },
+  {
+    id: 'calendar-03',
+    title: 'Evento de donacion',
+
+  },
+  {
+    id: 'calendar-04',
+    title: 'Junta semanal de equipo',
+
+  },
+  {
+    id: 'calendar-05',
+    title: 'Revision de pendientes con cliente',
+
+  },
+
+];
+const emails =[
+  {
+    id: 'email-01',
+    title: 'Reporte de resultados',
+
+  },
+  {
+    id: 'email-02',
+    title: 'Requisitos para cambio',
+
+  },
+  {
+    id: 'email-03',
+    title: 'Estatus de funcionalidad',
+
+  },
+  {
+    id: 'email-04',
+    title: 'Proximos eventos',
+
+  },
+  {
+    id: 'email-05',
+    title: 'Participa en la encuenta',
+
+  },
+
+];
+
+
 function App() {
   return (
     <div>
